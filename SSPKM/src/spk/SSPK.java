f911b22ff99284e199bd1a6e32ea132f7baba662
seleniumwdjj4@gmail.com <sachin.qa4it@gmail.com>
2016-08-20 16:39:40 +0530

    Second

--- SSPKM/src/spk/SSPK.java
+++ SSPKM/src/spk/SSPK.java
@@ -2,33 +2,29 @@ package spk;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class SSPK {
 
-    public static void main(String[] args) {
-        // declaration and instantiation of objects/variables
+	@Test
+    public  void main() {
+
         WebDriver driver = new FirefoxDriver();
         String baseUrl = "http://newtours.demoaut.com";
         String expectedTitle = "Welcome: Mercury Tours";
         String actualTitle = "";
 
-        // launch Firefox and direct it to the Base URL
         driver.get(baseUrl);
 
-        // get the actual value of the title
+
         actualTitle = driver.getTitle();
 
-        /*
-         * compare the actual title of the page witht the expected one and print
-         * the result as "Passed" or "Failed"
-         */
         if (actualTitle.contentEquals(expectedTitle)){
             System.out.println("Test Passed!");
         } else {
             System.out.println("Test Failed");
         }
        
-        //close Firefox
         driver.close();
        
         // exit the program explicitly
